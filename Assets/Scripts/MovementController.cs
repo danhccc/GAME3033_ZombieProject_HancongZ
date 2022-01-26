c37e3a19c7f6666969370087f0a81ca3956f3fab
Hancong Zhang <daniel94126@hotmail.com>
2022-01-26 15:11:51 -0500

    Week 3 - Lab submission

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -17,15 +17,21 @@ public class MovementController : MonoBehaviour
     PlayerController playerController;
     Rigidbody rigidbody;
     Animator playerAnimator;
+    public GameObject followTransform;
 
     //Movement Reference
     Vector2 inputVector = Vector2.zero;
     Vector3 moveDirection = Vector3.zero;
+    Vector2 lookInput = Vector2.zero;
+
+    public float aimSensitivity;
 
     public readonly int movementXHash = Animator.StringToHash("MovementX");
     public readonly int movementYHash = Animator.StringToHash("MovementY");
     public readonly int isJumpingHash = Animator.StringToHash("isJumping");
     public readonly int isRunningHash = Animator.StringToHash("isRunning");
+    public readonly int isFiringHash = Animator.StringToHash("IsFiring");
+    public readonly int isReloadingHash = Animator.StringToHash("IsReloading");
 
     private void Awake()
     {
@@ -42,6 +48,31 @@ public class MovementController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Camera X_Axis rotation
+        followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.x * aimSensitivity, Vector3.up);
+        followTransform.transform.rotation *= Quaternion.AngleAxis(lookInput.y * aimSensitivity, Vector3.left);
+        // Camera Y_Axis rotation
+        var angles = followTransform.transform.localEulerAngles;
+        angles.z = 0;
+
+        var angle = followTransform.transform.localEulerAngles.x;
+
+        if (angle > 180 && angle < 300)
+        {
+            angles.x = 300;
+        }
+        else if (angle < 180 && angle > 70)
+        {
+            angles.x = 70;
+        }
+
+        followTransform.transform.localEulerAngles = angles;
+
+
+        transform.rotation = Quaternion.Euler(0, followTransform.transform.rotation.eulerAngles.y, 0);
+        followTransform.transform.localEulerAngles = new Vector3(angles.x, 0f, 0f);
+
+        /**********************************************************/
 
         if (playerController.isJumping)
             return;
@@ -74,18 +105,44 @@ public class MovementController : MonoBehaviour
 
     public void OnJump(InputValue value)
     {
-        if (playerController.isJumping)
-            return;
+        //if (playerController.isJumping)
+        //    return;
 
         playerController.isJumping = true;
         rigidbody.AddForce((transform.up + moveDirection) * jumpForce, ForceMode.Impulse);
         playerAnimator.SetBool(isJumpingHash, playerController.isJumping);
     }
 
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+        // If we aim up, down, adjust animations to havew a mask that will let us properly animate aim
+
+    }
+
+    public void OnAim(InputValue value)
+    {
+        playerController.isAiming = value.isPressed;
+    }
+
+    public void OnFire(InputValue value)
+    {
+        playerController.isFiring = value.isPressed;
+        playerAnimator.SetBool(isFiringHash, playerController.isFiring);
+    }
+
+    public void OnReload(InputValue value)
+    {
+        playerController.isReloading = value.isPressed;
+        playerAnimator.SetBool(isReloadingHash, playerController.isReloading);
+    }
+
+
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Ground") && !playerController.isJumping)
             return;
         playerController.isJumping = false;
+        playerAnimator.SetBool(isJumpingHash, false);
     }
 }
